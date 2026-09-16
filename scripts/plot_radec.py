8f8611413776d6595526847597ff5af1f3ebec22
ZevVladimir <zevvladimir3002@gmail.com>
2026-09-16 16:21:50 -0700

    fix aesthetics of SFR ra/dec plot

--- scripts/plot_radec.py
+++ scripts/plot_radec.py
@@ -2,7 +2,19 @@
 from amms.core.analysis.maps import Map2D
 from amms.core.plotting.maps import show_map
 import matplotlib.pyplot as plt
+import numpy as np
+from pathlib import Path
+
+FIGS = Path(__file__).resolve().parent.parent / "figures"
+FIGS.mkdir(exist_ok=True)
+out = FIGS / "radec.png"
 
 m = Map2D.load("/xdisk/gbesla/zvladimir/products/b12_model2/lmc_069_sfr_radec_dt100myr.npz")
-show_map(m, log=True, min_counts=1)
-plt.savefig("radec.png", dpi=150, bbox_inches="tight")
+
+lon0, lon1, lat0, lat1 = m.extent
+dec_center = (lat0 + lat1) / 2.0
+aspect = 1.0 / np.clos(np.radians(dec_center))
+
+show_map(m, log=True, min_counts=1, aspect=aspect)
+
+plt.savefig(out , dpi=150, bbox_inches="tight")
